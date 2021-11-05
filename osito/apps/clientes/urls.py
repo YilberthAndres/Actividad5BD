364714545ff2907fd8cf1fc162a282ac82190a22
YilberthAndres <yilberthgalarza444@gmail.com>
2021-11-05 12:31:07 -0500

    yilberth

--- osito/apps/clientes/urls.py
+++ osito/apps/clientes/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from apps.clientes.views import index
+
 app_name = "ventas";
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('nuevo/', clientCreate, name='clientecreate'),
 ]
