fc9887862cf8024054ec75a06bd058076659e800
YilberthAndres <yilberthgalarza444@gmail.com>
2021-11-05 13:03:11 -0500

    yilberth

---
+++ osito/apps/clientes/form.py
@@ -0,0 +1,27 @@
+from apps.clientes.models import Cliente
+from django import forms
+
+class ClienteForm(forms.ModelForm):
+    class Meta:
+        model = Cliente
+
+        fields = [
+            'nombre',
+            'apellidos',
+            'direccion',
+            'telefono'
+        ]
+
+        labels = {
+            'nombre':    'Nombre',
+            'apellidos': 'Apellidos',
+            'direccion': 'Direccion',
+            'telefono':  'telefono'
+        }
+
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control'}),
+            'apellidos': forms.TextInput(attrs={'class': 'form-control'}),
+            'direccion': forms.TextInput(attrs={'class': 'form-control'}),
+            'telefono': forms.TextInput(attrs={'class': 'form-control'})
+        }
