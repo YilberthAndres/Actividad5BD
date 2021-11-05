59913d278a8b3d59c319fb20b153eb7d97789dfe
YilberthAndres <yilberthgalarza444@gmail.com>
2021-11-05 11:48:04 -0500

    yilberth

--- osito/apps/clientes/views.py
+++ osito/apps/clientes/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from apps.clientes.models import Cliente
 # Create your views here.
+
+def index(request):
+    cliente = Cliente.objects.all().order_by('-id')
+    context = {'clientes': cliente}
+    return render(request, 'clientes/listar.html', context)
