19ea1c576c7d78b12ffe7f4d151fdea5f5d38200
YilberthAndres <yilberthgalarza444@gmail.com>
2021-11-05 12:12:46 -0500

    yilberth

--- osito/apps/clientes/views.py
+++ osito/apps/clientes/views.py
@@ -6,3 +6,7 @@ def index(request):
     cliente = Cliente.objects.all().order_by('-id')
     context = {'clientes': cliente}
     return render(request, 'clientes/listar.html', context)
+
+def home(request):
+
+    return render(request, 'base/base.html')
--- osito/osito/urls.py
+++ osito/osito/urls.py
@@ -15,8 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from apps.clientes.views import home
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
     path('clientes/',  include('apps.clientes.urls',  namespace="clientes")),
 ]
