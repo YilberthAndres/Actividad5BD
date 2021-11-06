b7d7599211a046a713ff2be1d4addebdb0b189d5
leinys <lepagifo@hotmail.com>
2021-11-06 13:14:04 -0500

    leinys

--- osito/apps/proveedores/admin.py
+++ osito/apps/proveedores/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
+from apps.proveedores.models import Proveedore 
+
 # Register your models here.
+
+
+
+
+
+class ProveedoreAdmin(admin.ModelAdmin):
+      
+      list_display = ('nombre', 'apellido', 'direccion', 'provincia', 'telefono')
+      ordering = ('nombre', 'apellido', 'direccion', 'provincia', 'telefono')
+      search_fields = ('nombre', 'apellido', 'direccion', 'provincia', 'telefono')
+
+
+
+
+admin.site.register(Proveedore, ProveedoreAdmin )
